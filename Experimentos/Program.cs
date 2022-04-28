90c4630089d229a947e2d7868002e924d0fedb87
EDUARDO HARTWIG VIEIRA <eduardo.vieira3@alunos.sc.senac.br>
2022-04-27 21:55:42 -0300

    commit

---
+++ Experimentos/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Miniguerreiros
+{
+    class Program
+    {
+
+         
+
+        static void Main(string[] args)
+        {
+            int guerreiroHP = 100;
+            int imput;
+            string[] skillID = new string[10];
+            string skillNome;
+
+            static int ataqueB(int x)
+            {
+                int dano = 10;
+                return x - dano;
+            }
+
+
+
+            Habilidades h = new Habilidades();
+
+            h.Vida = guerreiroHP;
+            //h.AtaqueB = 
+            
+
+            Console.WriteLine("Qual Habilidade deseja usar ?");
+            Console.WriteLine("0 - Ataque básico.");
+            imput = Convert.ToInt32(Console.ReadLine());
+
+           switch(imput)
+            {
+
+                case 0:
+
+                    skillNome = "Ataque Básico";
+                    skillID[0] = skillNome;
+
+                    guerreiroHP = ataqueB(guerreiroHP);
+
+                    Console.WriteLine($"GuerreiroA fico com {guerreiroHP} de vida.");
+
+
+
+
+                    break;
+
+
+
+                default:
+
+
+
+
+                    break;
+
+
+            }
+
+
+
+        }
+    }
+}
